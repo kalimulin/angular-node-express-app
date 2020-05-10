eecbd326d9f2edc9e30775fbe3e9ccac48176c5a
kalimulin <kalimulin-ru@yandex.ru>
2020-05-10 12:36:10 +0400

    Подключение cors и morgan

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/auth')
@@ -7,8 +8,10 @@ const orderRoutes = require('./routes/auth')
 const positionRoutes = require('./routes/auth')
 const app = express()
 
+app.use(require('morgan')('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(require('cors')())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
