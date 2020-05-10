332dd2058d3417e33edeeacb231f5b7c8abf8582
kalimulin <kalimulin-ru@yandex.ru>
2020-05-10 12:24:32 +0400

    Парсинг пользователских данных

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/auth')
 const categoryRoutes = require('./routes/auth')
 const orderRoutes = require('./routes/auth')
 const positionRoutes = require('./routes/auth')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
