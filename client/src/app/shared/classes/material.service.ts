bb1c09340046c2804999c415de5afe30332c5256
kalimulin <kalimulin-ru@yandex.ru>
2020-05-15 15:26:59 +0400

    Создание макета сайта

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,7 +1,13 @@
+import {ElementRef} from "@angular/core";
+
 declare var M
 
 export class MaterialService {
   static toast(message: String) {
     M.toast({html: message})
+  }
+
+  static InitializeFloatingButton(ref: ElementRef) {
+    M.FloatingActionButton.init(ref.nativeElement)
   }
 }
