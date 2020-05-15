ab01164ef414945b4fafba79219b5ba22f9427f8
kalimulin <kalimulin-ru@yandex.ru>
2020-05-15 22:32:16 +0400

    Реализация вывода списка категорий

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,7 +14,8 @@ import {OverviewPageComponent} from './overview-page/overview-page.component'
 import {AnalyticsPageComponent} from './analytics-page/analytics-page.component'
 import {HistoryPageComponent} from './history-page/history-page.component'
 import {OrderPageComponent} from './order-page/order-page.component'
-import {CategoriesPageComponent} from './categories-page/categories-page.component'
+import {CategoriesPageComponent} from './categories-page/categories-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component'
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import {CategoriesPageComponent} from './categories-page/categories-page.compone
     AnalyticsPageComponent,
     HistoryPageComponent,
     OrderPageComponent,
-    CategoriesPageComponent
+    CategoriesPageComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
