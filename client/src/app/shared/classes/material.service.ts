cf987b0bb9f15334a00e17e3cddba60721dbf805
kalimulin <kalimulin-ru@yandex.ru>
2020-05-16 22:00:17 +0400

    добавление и редактирование категорий

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -10,4 +10,8 @@ export class MaterialService {
   static InitializeFloatingButton(ref: ElementRef) {
     M.FloatingActionButton.init(ref.nativeElement)
   }
+
+  static updateTextInputs() {
+    M.updateTextFields()
+  }
 }
