93523ca11f424d604050af3c7fecfc998d2df82a
kalimulin <kalimulin-ru@yandex.ru>
2020-05-17 12:13:32 +0400

    добавление и редактирование категорий

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {switchMap} from "rxjs/operators";
 import {of} from "rxjs";
 import {MaterialService} from "../../shared/classes/material.service";
+import {Category} from "../../shared/interfaces";
 
 @Component({
   selector: 'app-categories-form',
@@ -13,7 +14,11 @@ import {MaterialService} from "../../shared/classes/material.service";
 })
 export class CategoriesFormComponent implements OnInit {
 
+  @ViewChild('file') inputRef: ElementRef
   form: FormGroup
+  image: File
+  imagePreview: string | ArrayBuffer = ''
+  category: Category
   isNew = true
 
   constructor(private route: ActivatedRoute, private categoriesService: CategoriesService) { }
@@ -37,17 +42,55 @@ export class CategoriesFormComponent implements OnInit {
       )
       .subscribe(category => {
         if (category) {
+          this.category = category
           this.form.patchValue({
             name: category.name
           })
+          this.imagePreview = category.imageSrc
           MaterialService.updateTextInputs()
         }
         this.form.enable()
       }, error => MaterialService.toast(error.error.message))
   }
 
-  onSubmit() {
+  triggerClick() {
+    this.inputRef.nativeElement.click()
+  }
 
+  onFileUpload(event: Event) {
+    // @ts-ignore
+    const file = event.target.files[0]
+    this.image = file
+
+    const reader = new FileReader()
+    reader.onload = () => {
+      this.imagePreview = reader.result
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  onSubmit() {
+    let obs$
+    this.form.disable()
+
+    if (this.isNew) {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image)
+    } else {
+      obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image)
+    }
+
+    obs$.subscribe(
+      category => {
+        this.category = category
+        MaterialService.toast('Изменения сохранены')
+        this.form.enable()
+      },
+      error => {
+        MaterialService.toast(error.error.message)
+        this.form.enable()
+      }
+    )
   }
 
 }
