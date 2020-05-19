f6ed5901fc952e2a83804f2d6ec3c248c8986cdd
kalimulin <kalimulin-ru@yandex.ru>
2020-05-19 13:41:45 +0400

    Добавление, редактирование и удаление позиций

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChil
 import {PositionsService} from "../../../shared/services/positions.service";
 import {Position} from "../../../shared/interfaces";
 import {MaterialInstance, MaterialService} from "../../../shared/classes/material.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-positions-form',
@@ -14,11 +15,17 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   @ViewChild('modal') modalRef: ElementRef
   positions: Position[] = []
   loading = false
+  positionId = null
   modal: MaterialInstance
+  form: FormGroup
 
   constructor(private positionsService: PositionsService) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      cost: new FormControl(1, [Validators.required, Validators.min(1)])
+    })
     this.loading = true
     this.positionsService.fetch(this.categoryId)
       .subscribe(positions => {
@@ -36,15 +43,87 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   }
 
   onSelectPosition(position: Position) {
+    this.positionId = position._id
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    })
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
   onAddPosition() {
+    this.positionId = null
+    this.form.reset({
+      name: null,
+      cost: 1
+    })
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
   onCancel() {
     this.modal.close()
+  }
+
+  onSubmit() {
+    this.form.disable()
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+    if (this.positionId) {
+      newPosition._id = this.positionId
+      this.positionsService.update(newPosition)
+        .subscribe(
+          position => {
+            this.positions[this.positions.findIndex(p => p._id === position._id)] = position
+            MaterialService.toast('Позиция обновлена')
+            this.modal.close()
+          },
+          error => {
+            MaterialService.toast(error.error.message)
+          },
+          () => {
+            this.form.enable()
+            this.form.reset({name: '', post: 1})
+          }
+        )
+    } else {
+      this.positionsService.create(newPosition)
+        .subscribe(
+          position => {
+            MaterialService.toast('Позиция создана')
+            this.positions.push(position)
+            this.modal.close()
+          },
+          error => {
+            MaterialService.toast(error.error.message)
+          },
+          () => {
+            this.form.enable()
+            this.form.reset({name: '', post: 1})
+          }
+        )
+    }
+  }
+
+  onDeletePosition(event: Event, position: Position) {
+    event.stopPropagation()
+    const decision = window.confirm('Удалить позицию?')
+    if (decision) {
+      this.positionsService.delete(position).subscribe(
+        res => {
+          const i = this.positions.findIndex(p => p._id === position._id)
+          this.positions.splice(i, 1)
+          MaterialService.toast(res.message)
+        },
+        err => MaterialService.toast(err.error.message)
+      )
+    } else {
+
+    }
   }
 
 }
--- client/src/app/shared/services/positions.service.ts
+++ client/src/app/shared/services/positions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Position} from "../interfaces";
+import {Message, Position} from "../interfaces";
 import {Observable} from "rxjs";
 
 @Injectable({
@@ -11,5 +11,17 @@ export class PositionsService {
 
   fetch(categoryId: string): Observable<Position[]> {
     return this.http.get<Position[]>(`/api/position/${categoryId}`)
+  }
+
+  create(position: Position): Observable<Position> {
+    return this.http.post<Position>('/api/position', position)
+  }
+
+  update(position: Position): Observable<Position> {
+    return this.http.patch<Position>(`/api/position/${position._id}`, position)
+  }
+
+  delete(position: Position): Observable<Message> {
+    return this.http.delete<Message>(`/api/position/${position._id}`)
   }
 }
